90d6be48b38afa85dade5503dbc5fd62a6e42fed
Murray Heymann <heymann.murray@gmail.com>
2022-05-23 15:42:38 +0200

    Point to PFD2 project for now

--- packages/pfd/package.py
+++ packages/pfd/package.py
@@ -11,14 +11,15 @@ class Pfd(CMakePackage):
 
     homepage = "https://github.com/singular-gpispace/PFD"
     url      = "https://github.com/singular-gpispace/PFD/archive/refs/tags/v0.02.tar.gz"
-    git      = "https://github.com/singular-gpispace/PFD"
+    git      = "git@github.com:singular-gpispace/PFD2"
 
     maintainers = ['MHeymann', 'jankoboehm', 'lristau', 'mrahn']
 
+    version('latest', branch='public_candidate')
     version('0.02', sha256='945e8419258f53bd29dc03aa6b2f36caa92900f4f9893747e28683d76df20653')
 
     depends_on('singular@snapshot_22_03')
-    depends_on('gpi-space@21.09')
+    depends_on('gpi-space@22.03')
     depends_on('flint@2.6.3')
 
     def cmake_args(self):
