0f3c4517562129c839669c886f9aea94e35ab149
gnawali55 <gewalis55@gmail.com>
2024-11-05 16:30:55 +0100

    Added modular_res

---
+++ packages/modular_res/package.py
@@ -0,0 +1,36 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Modular(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://github.com/singular-gpispace/modular_res"
+    git      = "https://github.com/singular-gpispace/modular_res"
+
+    maintainers = ['hobihsina','jankoboehm','Santosh']
+
+    version('latest', branch='main')
+
+    depends_on('singular@snapshot_22_03')
+    depends_on('gpi-space@22.06:')
+    depends_on('flint@2.6.3:')
+
+    def cmake_args(self):
+        spec = self.spec
+        print(self.spec)
+        args = [ self.define("GSPC_HOME", spec['gpi-space'].prefix)
+               , self.define("SINGULAR_HOME", spec['singular'].prefix)
+               , self.define("FLINT_HOME", spec['flint'].prefix)
+               ]
+        return args
+
+    def setup_run_environment(self, env):
+        spec = self.spec
+        env.set('SINGULAR_INSTALL_DIR', spec['singular'].prefix)
+        env.set('MODULAR_INSTALL_DIR', self.prefix)
+
